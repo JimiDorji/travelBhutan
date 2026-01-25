4b8ff4cb7e08580fd9a76ff79a9dd8fa238e48cf
JimiDorji <jigmedorji1000@gmail.com>
2026-01-25 14:02:55 +0600

    google ads

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Mhome from '../src/mhome/main';
+import Script from 'next/script'
+import Mhome from '../src/mhome/main'
 
 export default function Home() {
   return (
-    <Mhome />
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9681042570650797"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
+      <Mhome />
+    </>
   )
 }
